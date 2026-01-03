9803850b159b7fa703cc20f1717f300c351cad28
Kevin Huber <ItsKev@users.noreply.github.com>
2026-01-03 15:27:59 +0000

    chore: use grounds gradle plugin (#14)

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -1,29 +1,3 @@
-plugins {
-    id("com.gradleup.shadow") version "9.3.0"
-    id("com.github.gmazzo.buildconfig") version "6.0.7"
-}
+plugins { id("gg.grounds.velocity") version "0.1.0" }
 
-dependencies {
-    implementation(project(":common"))
-    compileOnly("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
-    kapt("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
-}
-
-tasks.build { dependsOn(tasks.shadowJar) }
-
-tasks.jar { enabled = false }
-
-tasks.shadowJar {
-    archiveBaseName.set(rootProject.name)
-    archiveClassifier.set("") // Removes the 'all' classifier
-    archiveVersion.set("") // Removes the version from the jar name
-}
-
-buildConfig {
-    className("BuildInfo")
-    packageName("gg.grounds")
-
-    useKotlinOutput()
-
-    buildConfigField("String", "VERSION", "\"${project.version}\"")
-}
+dependencies { implementation(project(":common")) }
