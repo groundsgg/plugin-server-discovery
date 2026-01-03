faf55d636c8c1cd717987acbd07ac03ef2af5f24
ItsKev <kevin.huber@hotmail.com>
2026-01-03 14:39:17 +0100

    chore: use grounds gradle plugin

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,24 +1,3 @@
-plugins { id("com.gradleup.shadow") version "9.3.0" }
+plugins { id("gg.grounds.paper") version "0.1.0" }
 
-dependencies {
-    implementation(project(":common"))
-    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
-}
-
-tasks.build { dependsOn(tasks.shadowJar) }
-
-tasks.jar { enabled = false }
-
-tasks.shadowJar {
-    archiveBaseName.set(rootProject.name)
-    archiveClassifier.set("") // Removes the 'all' classifier
-    archiveVersion.set("") // Removes the version from the jar name
-}
-
-val pluginVersion = project.version.toString()
-
-tasks.processResources {
-    inputs.property("version", project.version)
-
-    filesMatching(listOf("**/plugin.yml")) { expand("VERSION" to pluginVersion) }
-}
+dependencies { implementation(project(":common")) }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,16 @@
 rootProject.name = "plugin-server-discovery"
 
 include("common", "velocity", "paper")
+
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/groundsgg/*")
+            credentials {
+                username = providers.gradleProperty("github.user").get()
+                password = providers.gradleProperty("github.token").get()
+            }
+        }
+        gradlePluginPortal()
+    }
+}
